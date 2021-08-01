96c0814ae5eb237923ec4f86b3314957955b34a0
shreykoradia <shrey.koradia@yahoo.com>
2021-08-01 10:58:43 +0530

    made use of axios and made the application Real data look in the cards structure which was made by material Ui

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import {Cards , Charts , Countrypicker } from './components'
 import {fetchData} from './api'
 
 class App extends React.Component {
-
+state = {
+  data:{},
+}
 async componentDidMount(){
-  const data = await fetchData();
-  console.log(data);
+  const fetchedData = await fetchData();
+  this.setState({data : fetchedData})
 }
   render(){
+    const {data} = this.state;
     return (
       <div className = "container">
           
-          <Cards />
+          <Cards data={data} />
           <Countrypicker />
           <Charts />
           
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
+import {Card , CardContent ,Typography , Grid} from '@material-ui/core'
+import styles from './cards.modules.css'
+import CountUp from 'react-countup'
 
-const Cards = () =>{
+
+const Cards = ({data:{confirmed , recovered , deaths , lastUpdate} }) =>{
+ 
+    if(!confirmed){
+        return 'Loading....'
+  }
     return(
-        <h1>cards</h1>
-    )
+       <div className="styles container">
+           <Grid container spacing ={3} justifyContent='center'>
+               <Grid item component={Card}>
+                   <CardContent>
+                       <Typography color='textSecondary' gutterBottom>Infected</Typography>
+                       <Typography variant='h5'>
+                           <CountUp 
+                            start ={0}
+                            end={confirmed.value}
+                            duration={2.5}
+                            separator=','
+                           />
+                       </Typography>
+                       <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
+                       <Typography variant='body2'>Number of Active cases of Covid-19</Typography>
+                   </CardContent>
+               </Grid>
+
+               <Grid item component={Card}>
+                   <CardContent>
+                       <Typography color='textSecondary' gutterBottom>Recoveries</Typography>
+                       <Typography variant='h5'>
+                       <CountUp 
+                            start ={0}
+                            end={recovered.value}
+                            duration={2.5}
+                            separator=','
+                           />
+                       </Typography>
+                       <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
+                       <Typography variant='body2'>Number of Recovered cases of Covid-19</Typography>
+                   </CardContent>
+               </Grid>
+
+               <Grid item component={Card}>
+                   <CardContent>
+                       <Typography color='textSecondary' gutterBottom>Deaths</Typography>
+                       <Typography variant='h5'>
+                       <CountUp 
+                            start ={0}
+                            end={deaths.value}
+                            duration={2.5}
+                            separator=','
+                           />
+                       </Typography>
+                       <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
+                       <Typography variant='body2'>Number of Deaths from Covid-19</Typography>
+                   </CardContent>
+               </Grid>
+
+           </Grid>
+       </div>
+      
+    );
 }
 
 export default Cards;
